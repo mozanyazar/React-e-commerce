0c4f62f2facd28887a71767e238c4c7dbcb9e95e
mozanyazar <mahmutozanyazar@gmail.com>
2023-02-19 22:17:51 +0300

    add wishlist and basket snapshot when user login

--- src/store/Auth.jsx
+++ src/store/Auth.jsx
@@ -9,19 +9,20 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, setDoc } from "firebase/firestore";
 import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useLocalStorageState from "use-local-storage-state";
 import { auth, db } from "../firebase";
 const Auth = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
   const [user, setUser] = useLocalStorageState("user", { defaultValue: null });
   const [message, setMessage] = useState({
     isSucces: false,
     message: undefined,
   });
-
   const [basket, setBasket] = useState([]);
   const [wishList, setWishList] = useState([]);
 
@@ -29,6 +30,8 @@ export const AuthProvider = ({ children }) => {
     onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
         setUser(currentUser);
+        wishListSnapShot(currentUser.uid);
+        basketListSnapShot(currentUser.uid);
       } else {
         setUser(null);
         setWishList([]);
@@ -37,8 +40,26 @@ export const AuthProvider = ({ children }) => {
     });
   }, []);
 
-  // sign out
+  // wishlist snapshot login and signup
+  const wishListSnapShot = async (userId) => {
+    const docRef = doc(db, "wishlist", userId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log("wish");
+      setWishList(docSnap.data().wishlist);
+    }
+  };
+  // basket snapshot login and signup
+  const basketListSnapShot = async (userId) => {
+    const docRef = doc(db, "basket", userId);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log("basket");
+      setBasket(docSnap.data().basket);
+    }
+  };
 
+  // sign out
   const logOut = async () => {
     await signOut(auth)
       .then(() => {
@@ -46,6 +67,7 @@ export const AuthProvider = ({ children }) => {
           isSucces: true,
           message: "successful",
         });
+        navigate("/");
       })
       .catch((e) => {
         setMessage({
@@ -58,8 +80,12 @@ export const AuthProvider = ({ children }) => {
   // create a collections wishlist, basket
   const createCollections = async (user) => {
     try {
-      await setDoc(doc(db, "wishlist", user.user.uid), {});
-      await setDoc(doc(db, "basket", user.user.uid), {});
+      await setDoc(doc(db, "wishlist", user.user.uid), {
+        wishlist: [],
+      });
+      await setDoc(doc(db, "basket", user.user.uid), {
+        basket: [],
+      });
     } catch (e) {
       console.log(e.message);
     }
@@ -70,7 +96,7 @@ export const AuthProvider = ({ children }) => {
   const signInFn = async (email, password) => {
     console.log(email);
     try {
-      await signInWithEmailAndPassword(auth, email, password).then(() => {
+      await signInWithEmailAndPassword(auth, email, password).then((user) => {
         setMessage({
           isSucces: true,
           message: "Sign In Successful ",
